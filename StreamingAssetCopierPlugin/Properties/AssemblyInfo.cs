3f5efda7a1e24ceaf81e4c10f9c134beb0c348f8
Jeremy Pritts <jrpritts7@gmail.com>
2021-10-17 17:20:34 -0400

    Add project files.

---
+++ StreamingAssetCopierPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StreamingAssetCopierPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StreamingAssetCopierPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5EEC6823-FC07-4E24-84EF-745223CE96D9")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssetRipper.Library.Attributes.RegisterPlugin(typeof(StreamingAssetCopierPlugin.StreamingPlugin))]
---
+++ StreamingAssetCopierPlugin/StreamingPlugin.cs
@@ -0,0 +1,56 @@
+﻿using AssetRipper.Library;
+using System.IO;
+using System.Linq;
+
+namespace StreamingAssetCopierPlugin
+{
+	internal class StreamingPlugin : PluginBase
+	{
+		public static readonly string[] SupportedFileExtensions = new string[] { ".png", ".jpg", ".mp3", ".ogg", ".wav", ".mp4" };
+		public const string OutputFolderName = "StreamingAssetsCopied";
+
+		public override string Name => "StreamingCopier";
+
+		public override void Initialize()
+		{
+			CurrentRipper.OnFinishExporting += new System.Action(DoPostExport);
+		}
+
+		private void DoPostExport()
+		{
+			Info($"Copying compatible streaming assets");
+			string streamingAssetsPath = CurrentRipper.GameStructure?.PlatformStructure?.StreamingAssetsPath;
+			string exportPath = CurrentRipper.Settings.ExportPath;
+			if (string.IsNullOrEmpty(streamingAssetsPath) || string.IsNullOrEmpty(exportPath) || !Directory.Exists(streamingAssetsPath))
+				return;
+
+			string[] streamingFiles = Directory.GetFiles(streamingAssetsPath, "*", SearchOption.AllDirectories).Where(path => HasCompatibleExtension(path)).ToArray();
+			if (streamingFiles.Length == 0)
+				return;
+
+			Info($"Found {streamingFiles.Length} compatible files to copy");
+			string outputPath = Path.Combine(exportPath, OutputFolderName);
+			foreach(string file in streamingFiles)
+			{
+				string relativePath = Path.GetRelativePath(streamingAssetsPath, file);
+				string newPath = Path.Combine(outputPath, relativePath);
+				string newDirectory = Path.GetDirectoryName(newPath);
+				Info($"Copying to {newPath}");
+				Directory.CreateDirectory(newDirectory);
+				File.Copy(file, newPath);
+			}
+		}
+
+		private static bool HasCompatibleExtension(string path)
+		{
+			if (string.IsNullOrEmpty(path))
+				return false;
+			foreach(var extension in SupportedFileExtensions)
+			{
+				if (path.EndsWith(extension))
+					return true;
+			}
+			return false;
+		}
+	}
+}
